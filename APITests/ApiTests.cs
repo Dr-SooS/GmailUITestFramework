2547409fa7f2c3ab63396e6f2ca7b4177ab08337
Vladislav Dobritski <Vladislav_Dobritski@epam.com>
2018-07-25 13:20:54 +0300

    Api tests

---
+++ APITests/ApiTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using APITests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace APITests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        public HttpClient Client = new HttpClient();
+
+        [TestMethod]
+        public void StatusCodeTest()
+        {
+            var response = Client.GetAsync("https://jsonplaceholder.typicode.com/users").Result;
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void HeaderTest()
+        {
+            var response = Client.GetAsync("https://jsonplaceholder.typicode.com/users").Result;
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [TestMethod]
+        public void ContentTest()
+        {
+
+            var response = Client.GetAsync("https://jsonplaceholder.typicode.com/users").Result;
+            var result = JsonConvert.DeserializeObject<List<User>>(response.Content.ReadAsStringAsync().Result);
+            Assert.AreEqual(10, result.Count);
+        }
+    }
+}
---
+++ APITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("APITests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9455e72-c189-42ef-8143-e797ff88ef28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
